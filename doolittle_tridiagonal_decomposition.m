4eb0f7c6ae29fe6c7cf7307e0acaf37c24912ebc
Muhamad Ali Khan <72930799+muhammadalizkhan@users.noreply.github.com>
2023-09-02 20:12:04 +0500

    Create doolittle_tridiagonal_decomposition.m

---
+++ doolittle_tridiagonal_decomposition.m
@@ -0,0 +1,22 @@
+function [L, U] = doolittle_tridiagonal_decomposition(A)
+    % Check if the input matrix is tridiagonal
+    [m, n] = size(A);
+    if m ~= n || any(diag(A, -1) == 0) || any(diag(A, 1) == 0)
+        error('Input matrix is not tridiagonal');
+    end
+    
+    n = length(A);
+    L = eye(n); % Initialize L as an identity matrix
+    U = zeros(n); % Initialize U as an all-zero matrix
+
+    % Perform LU decomposition
+    for k = 1:n
+        U(k, k) = A(k, k);
+        L(k, k) = 1;
+        if k < n
+            U(k, k + 1) = A(k, k + 1);
+            L(k + 1, k) = A(k + 1, k) / U(k, k);
+            U(k + 1, k + 1) = A(k + 1, k + 1) - L(k + 1, k) * A(k, k + 1);
+        end
+    end
+end
